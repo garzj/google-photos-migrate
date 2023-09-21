42cdaaa91f18a086f2280985a80701bb84a41c3e
garzj <johannes@garz.dev>
2023-09-21 13:37:09 +0200

    Add support for .MP files [#9]

--- src/config/extensions.ts
+++ src/config/extensions.ts
@@ -1,6 +1,11 @@
 import { MetaType } from '../meta/MetaType';
 import { MediaFileExtension } from '../media/MediaFileExtension';
 
+// The extensions are identified by their suffix (longest match first)
+// A list of aliases will match more .json files (tried in order)
+
+const livePhotoAliases = ['.heic', '.jpg', '.jpeg'];
+
 export const supportedExtensions: MediaFileExtension[] = [
   { suffix: '.jpg', metaType: MetaType.EXIF },
   { suffix: '.jpeg', metaType: MetaType.EXIF },
@@ -12,16 +17,10 @@ export const supportedExtensions: MediaFileExtension[] = [
   { suffix: '.heic', metaType: MetaType.QUICKTIME },
   { suffix: '.heif', metaType: MetaType.QUICKTIME },
   { suffix: '.gif', metaType: MetaType.NONE },
-  {
-    suffix: '.mp4',
-    metaType: MetaType.QUICKTIME,
-    aliases: ['.heic', '.jpg', '.jpeg'], // Live photos
-  },
-  {
-    suffix: '.mov',
-    metaType: MetaType.QUICKTIME,
-    aliases: ['.heic', '.jpg', '.jpeg'], // Live photos
-  },
+  { suffix: '.mp4', metaType: MetaType.QUICKTIME, aliases: livePhotoAliases },
+  { suffix: '.mov', metaType: MetaType.QUICKTIME, aliases: livePhotoAliases },
+  { suffix: '.mp', metaType: MetaType.QUICKTIME, aliases: livePhotoAliases },
+  { suffix: '.mvimg', metaType: MetaType.QUICKTIME, aliases: livePhotoAliases },
   { suffix: '.qt', metaType: MetaType.QUICKTIME },
   { suffix: '.mov.qt', metaType: MetaType.QUICKTIME },
   { suffix: '.3gp', metaType: MetaType.QUICKTIME },
