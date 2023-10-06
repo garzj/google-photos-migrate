4988200e9e4e8d1f17d6d220923b25c8d25a8c13
garzj <johannes@garz.dev>
2023-10-06 22:59:31 +0200

    Remove rewriteAllTags subcmd

--- src/cli.ts
+++ src/cli.ts
@@ -3,14 +3,12 @@
 import { subcommands, run } from 'cmd-ts';
 import { migrateFull } from './commands/migrate-full';
 import { migrateFlat } from './commands/migrate-flat';
-import { rewriteAllTags } from './commands/rewrite-all-tags';
 
 const app = subcommands({
   name: 'google-photos-migrate',
   cmds: {
     full: migrateFull,
     flat: migrateFlat,
-    rewriteAllTags,
   },
 });
 
