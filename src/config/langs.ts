10b4633cb0da4e15e086bee40e064b49c63e2e96
garzj <johannes@garz.dev>
2024-01-18 19:51:43 +0100

    Format files

--- src/config/langs.ts
+++ src/config/langs.ts
@@ -1,3 +1,7 @@
 export const editedSuffices = ['edited', 'bearbeitet', 'modifié', 'düzenlendi'];
-export const photosDirs = ['Google Photos', 'Google Fotos', 'Google Fotoğraflar'];
+export const photosDirs = [
+  'Google Photos',
+  'Google Fotos',
+  'Google Fotoğraflar',
+];
 export const untitledDirs = ['Untitled', 'Unbenannt', 'Adsız'];
