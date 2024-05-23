127d84c5615a3b63a6c6786687dad21e92c42814
garzj <johannes@garz.dev>
2024-05-23 10:33:23 +0200

    fix: export classes as values

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { migrateDirFlat, migrateDirFlatGen } from './dir/migrate-flat';
 import { migrateDirFull, migrateDirFullGen } from './dir/migrate-full';
 import type { MigrationArgs } from './dir/migration-args';
 
-export type * from './errors';
+export * from './errors';
 export type * from './media/MediaFile';
 export type * from './media/MediaFileExtension';
 export type * from './meta/GoogleMeta';
