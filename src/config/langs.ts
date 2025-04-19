3d28eebcd250ad37468dbe0b48d5190f44f21037
garzj <johannes@garz.dev>
2025-04-19 03:02:03 +0200

    refactor: restructure language configuration

--- src/config/langs.ts
+++ src/config/langs.ts
@@ -1,33 +1,63 @@
-export const editedSuffices = [
-  'edited',
-  'bearbeitet',
-  'modifié',
-  'düzenlendi',
-  'измененный',
-  'ha editado',
-  '編集済み',
-  '已修改',
-  'muokattu',
-  '已編輯',
-];
-export const photosDirs = [
-  'Google Photos',
-  'Google Fotos',
-  'Google Fotoğraflar',
-  'Google Фото',
-  'Google フォト',
-  'Google 相册',
-  'Google Kuvat',
-  'Google 相簿',
-];
-export const untitledDirs = [
-  'Untitled',
-  'Unbenannt',
-  'Adsız',
-  'Без названия',
-  'Sin título',
-  '無題',
-  '未命名',
-  'Nimetön',
-  '未命名',
-];
+export interface LocalizedTerm {
+  editedSuffix?: string;
+  photosDir?: string;
+  untitledDir?: string;
+}
+
+export const localizedTerms: Record<string, LocalizedTerm> = {
+  de: {
+    editedSuffix: 'bearbeitet',
+    photosDir: 'Google Fotos',
+    untitledDir: 'Unbenannt',
+  },
+  en: {
+    editedSuffix: 'edited',
+    photosDir: 'Google Photos',
+    untitledDir: 'Untitled',
+  },
+  es: {
+    editedSuffix: 'ha editado',
+    untitledDir: 'Sin título',
+  },
+  fi: {
+    editedSuffix: 'muokattu',
+    photosDir: 'Google Kuvat',
+    untitledDir: 'Nimetön',
+  },
+  fr: {
+    editedSuffix: 'modifié',
+  },
+  ja: {
+    editedSuffix: '編集済み',
+    photosDir: 'Google フォト',
+    untitledDir: '無題',
+  },
+  ru: {
+    editedSuffix: 'измененный',
+    photosDir: 'Google Фото',
+    untitledDir: 'Без названия',
+  },
+  tr: {
+    editedSuffix: 'düzenlendi',
+    photosDir: 'Google Fotoğraflar',
+    untitledDir: 'Adsız',
+  },
+  'zh-CN': {
+    editedSuffix: '已修改',
+    photosDir: 'Google 相册',
+    untitledDir: '未命名',
+  },
+  'zh-TW': {
+    editedSuffix: '已編輯',
+    photosDir: 'Google 相簿',
+    untitledDir: '未命名',
+  },
+};
+
+const getValues = (key: keyof LocalizedTerm) =>
+  Object.values(localizedTerms)
+    .map((t) => t[key])
+    .filter((v): v is Exclude<typeof v, undefined> => v !== undefined);
+export const photosDirs = getValues('photosDir');
+export const untitledDirs = getValues('untitledDir');
+export const editedSuffices = getValues('editedSuffix');
